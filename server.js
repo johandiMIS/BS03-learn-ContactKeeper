f52788f18427da5b273af97ef867dfa777346710
unknown <limjohandi@gmail.com>
2023-05-13 01:07:59 +0700

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Define Routes 
+app.use('/api/users', require('./routes/users'))
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/contact', require('./routes/contacts'))
+
+app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
