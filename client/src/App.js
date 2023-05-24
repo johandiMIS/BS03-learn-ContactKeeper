a1e596bf9d9352ee9fd1907e55c61a25230e8507
johandi <johandi.lim@satnusa.com>
2023-05-24 14:42:28 +0700

    login and register form, auth and alert contect, alert bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,34 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <div className="App"> 
-          <Navbar/>
-          <div className='container'>
-            <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/about' element={<About/>}/>
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
-    </ContactState>
+    <AlertState>
+      <AuthState>
+        <ContactState>
+          <BrowserRouter>
+            <div className="App"> 
+              <Navbar/>
+              <div className='container'>
+                <Alerts/>
+                <Routes>
+                  <Route path='/' element={<Home/>}/>
+                  <Route path='/about' element={<About/>}/>
+                  <Route path='/register' element={<Register/>}/>
+                  <Route path='/login' element={<Login/>}/>
+                </Routes>
+              </div>
+            </div>
+          </BrowserRouter>
+        </ContactState>
+      </AuthState>
+    </AlertState>
   )
   
 }
