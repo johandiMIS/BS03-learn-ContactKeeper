72fb43426dbe6d4d76e07303abe6d9c9d31df651
johandi <johandi.lim@satnusa.com>
2023-05-26 16:21:31 +0700

    preparation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -13,5 +13,14 @@ app.use(express.json({extended:false}))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contact', require('./routes/contacts'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
 
 app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
